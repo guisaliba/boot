99fda73c7ab275539e7b50b53c0afff8f4051267
Guilherme Saliba <salibagui19@gmail.com>
2025-01-02 01:31:07 -0300

    feat: chapters I and II from L1

---
+++ L1/variables.py
@@ -0,0 +1,7 @@
+sword_damage = 10
+player_health = 100
+health_after_attack = player_health - sword_damage
+
+print(f"Player's health is: {player_health}")
+print(f"Player got hit by a sword and took {sword_damage} damage!")
+print(f"Player's health is now: {health_after_attack}")
